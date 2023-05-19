81ef3cd3c515d372f6cd0acdbddb603c5fc081dc
mikejhouseman <mikej.houseman@gmail.com>
2023-05-18 21:23:44 -0700

    Test with removing column.2

--- backend/db/migrations/20230516032102-create-spots.js
+++ backend/db/migrations/20230516032102-create-spots.js
@@ -27,7 +27,7 @@ module.exports = {
       spotId: {
         type: Sequelize.INTEGER,
         allowNull: false
-      }
+      },
       // ,
       // reviewId: {
       //   type: Sequelize.INTEGER,
@@ -38,7 +38,7 @@ module.exports = {
       //   },
       //   onUpdate: 'CASCADE',
       //   onDelete: 'CASCADE'
-      },
+      // },
       imageId: {
         type: Sequelize.INTEGER,
         allowNull: false,
