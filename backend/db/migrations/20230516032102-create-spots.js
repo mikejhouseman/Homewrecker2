ece82b698dc3870f8c9ed49a08790f6f4268fb19
mikejhouseman <mikej.houseman@gmail.com>
2023-05-18 20:49:03 -0700

    Typo in user model attribute in create spots migration

--- backend/db/migrations/20230516032102-create-spots.js
+++ backend/db/migrations/20230516032102-create-spots.js
@@ -18,7 +18,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: 'users',
+          model: 'user',
           key: 'id'
         },
         onUpdate: 'CASCADE',
