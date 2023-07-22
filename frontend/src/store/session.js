48b339cdc8926f10360205b012ea7fad7aa34372
mikejhouseman <mikej.houseman@gmail.com>
2023-07-22 13:39:41 -0700

    Phase 1 Complete

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -19,7 +19,7 @@ const removeUser = () => {
 
 export const login = (user) => async (dispatch) => {
   const { credential, password } = user;
-  const response = await csrfFetch("/api/session", {
+  const response = await csrfFetch("/api/session/", {
     method: "POST",
     body: JSON.stringify({
       credential,
@@ -32,7 +32,7 @@ export const login = (user) => async (dispatch) => {
 };
 
 export const restoreUser = () => async (dispatch) => {
-  const response = await csrfFetch("/api/session");
+  const response = await csrfFetch("/api/session/");
   const data = await response.json();
   dispatch(setUser(data.user));
   return response;
