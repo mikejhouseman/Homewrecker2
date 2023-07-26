03fba10bc32c129540b12ef4cfa88312e59db76b
mikejhouseman <mikej.houseman@gmail.com>
2023-07-25 20:04:44 -0700

    Deploy to Heroku

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -17,12 +17,19 @@ function LoginFormPage() {
   const handleSubmit = (e) => {
     e.preventDefault();
     setErrors({});
-    return dispatch(sessionActions.login({ credential, password })).catch(
-      async (res) => {
-        const data = await res.json();
-        if (data && data.errors) setErrors(data.errors);
-      }
-    );
+
+    dispatch(sessionActions.login({ credential, password }))
+      .then((res) => {
+        if (res.status >= 400) {
+          return res.json().then((data) => {
+            if (data && data.errors) setErrors(data.errors);
+          });
+        }
+      })
+      .catch((err) => {
+        // Handle other errors here if needed
+        console.error("Error occurred during login:", err);
+      });
   };
 
   return (
