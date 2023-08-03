551f02ae9f00c5cff8ea9a9f2abd0298f41dd369
mikejhouseman <mikej.houseman@gmail.com>
2023-08-03 16:29:57 -0700

    Edits to AppHeader

--- frontend/src/components/AppHeader/index.js
+++ frontend/src/components/AppHeader/index.js
@@ -1,28 +1,21 @@
 // frontend/src/components/AppHeader/index.js
+// frontend/src/components/AppHeader/index.js
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import ProfileButton from '../Navigation/ProfileButton';
 import LoginFormModal from '../LoginFormModal';
 import SignupFormModal from '../SignupFormModal';
 import logoImage from '../assets/homewreckerLogo.png';
-import { hideAuthButtons, showAuthButtons } from '../../store/ui';
 import { useModal } from '../../context/Modal';
 import './AppHeader.css';
 
 const AppHeader = () => {
-  const authButtonsHidden = useSelector(state => state.ui.authButtonsHidden);
   const sessionUser = useSelector(state => state.session.user);
-  const dispatch = useDispatch();
   const { closeModal } = useModal();
 
   const toggleModal = () => {
     closeModal();
-    if (authButtonsHidden) {
-      dispatch(showAuthButtons());
-    } else {
-      dispatch(hideAuthButtons());
-    }
   };
 
   return (
@@ -40,12 +33,8 @@ const AppHeader = () => {
           </>
         ) : (
           <>
-            {!authButtonsHidden && (
-              <LoginFormModal onClick={toggleModal} />
-            )}
-            {!authButtonsHidden && (
-              <SignupFormModal onClick={toggleModal} />
-            )}
+            <LoginFormModal onClick={toggleModal} />
+            <SignupFormModal onClick={toggleModal} />
           </>
         )}
       </div>
@@ -54,3 +43,4 @@ const AppHeader = () => {
 };
 
 export default AppHeader;
+
